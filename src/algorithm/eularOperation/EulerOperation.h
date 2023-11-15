c97dbd79bb2716c54ecddd6dcd5f58983e9c4a99
登神长阶 <145771680+ArsNovaaa@users.noreply.github.com>
2023-11-15 22:38:01 +0800

    Update EulerOperation.h

--- src/algorithm/eularOperation/EulerOperation.h
+++ src/algorithm/eularOperation/EulerOperation.h
@@ -11,11 +11,11 @@ namespace euler {
 
 
 
-		Solid* MVFS(glm::vec3 pos);								//make vertex face solid
+		Solid* MVFS(glm::vec3 pos);	//make vertex face solid
 		HalfEdge* MEV(Loop* lp, Vertex* sv, glm::vec3 pos);	// make edge vertex
 		Loop* MEF(Loop* lp, Vertex* v1, Vertex* v2);		// make edge face
 		Loop* KEMR(Loop* lp, Vertex* v1, Vertex* v2); //make edge kill ring
-		void KFMRH(Loop* outerLoop, Loop* innerLoop);				// make face kill ring and hole
+		void KFMRH(Loop* outerLoop, Loop* innerLoop);	// make face kill ring and hole
 		Solid* Sweep(Face* f, glm::vec3 direction,float distance);
 		void InfoSolid(Solid* solid);
 		void InfoFace(Face* face);
